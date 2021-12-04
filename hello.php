bd2fca6439e8d16f995680473bb2b5b6d61d9a74
safouaneOulhabib <safouaneoulhabib@gmail.com>
2021-12-04 16:04:38 +0100

    third commit from samir

--- hello.php
+++ hello.php
@@ -4,12 +4,12 @@ echo "Hello World First commit";
 
 // $ git add . 
 // $ git commit -m "second commit"
-// $ git push -u origin master
+// $ git push -u origin samir
 echo "Hello World Second commit";
 
 // $ git add . 
 // $ git commit -m "third commit"
-// $ git push -u origin master
-echo "Hello World Third commit from master";
+// $ git push -u origin samir
+echo "Hello World Third commit from samir";
 
 ?>
